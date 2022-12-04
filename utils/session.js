d6b85933cf1e72228d1b787f1aede6e7ad454db4
Matt Gregg <mattdgregg@gmail.com>
2022-12-04 16:12:12 -0600

    upgrade

--- utils/session.js
+++ utils/session.js
@@ -2,7 +2,7 @@ import { withIronSessionApiRoute, withIronSessionSsr } from "iron-session/next";
 
 const sessionOptions = {
   password: process.env.SECRET_COOKIE_PASSWORD,
-  cookieName: "next-iron-session/examples/next.js",
+  cookieName: "iron-session/examples/next.js",
   // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
   cookieOptions: {
     secure: process.env.NODE_ENV === "production",
